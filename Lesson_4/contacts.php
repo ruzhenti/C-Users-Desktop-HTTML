04592873b06173f5dc846bb5a6e7ca8dd5fdc7e8
ruzhenti <99267013+ruzhenti@users.noreply.github.com>
2022-11-14 18:48:06 +0300

    Add files via upload

---
+++ Lesson_4/contacts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<div class="header">
+    <div class="menu">
+      <ul>
+        <li><a href="index.php">Обо мне</a></li>
+        <li><a href="achivments.php">Мои достиджения</a></li>
+        <li><a href="main.php">Мой опыт</a></li>
+        <li><a href="contacts.php">Мои контакты</a></li>
+      </ul>
+    </div>
+</div>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Мои контакты </title>
+  <link rel="shortcut icon" href="/images/favicon.png">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+<div class="container">
+  <h2>Мои контакты:</h2>
+    <h3> <p> Ирина Р.</p>
+    <p> Пушкино, Московской области</p>
+    <p> Email: time_to_change@yandex.ru</p>
+    <p> Tel:+79846543190</p> 
+    </h3>
+</div>
+<div class="footer">
+<p class="copyright"><h2>"До встречи!" </h2></p>
+</div>
+</html>
